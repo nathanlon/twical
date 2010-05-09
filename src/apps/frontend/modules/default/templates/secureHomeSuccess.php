f5be7d0e0e98902827cc990704b548ffc8f61f11
Nathan O'Hanlon <nathan@ProsperTechnologies.com>
2010-05-09 10:41:23 +0100

    latest

---
+++ src/apps/frontend/modules/default/templates/secureHomeSuccess.php
@@ -0,0 +1,6 @@
+<h1>Welcome to TwiCal.net - You are logged in</h1>
+
+<div>TwiCal allows you receive reminders via twitter for upcoming events in your Google Calendar</div>
+
+<div>Upload a ICS File here</div>
+
