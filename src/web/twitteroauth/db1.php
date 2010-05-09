15a996f88ed9c3fbcf9938ac989a08836b496ccd
Ketan Majmudar <ket@spiritquest.co.uk>
2010-05-09 14:12:26 +0100

    quick db hacks 2

--- src/web/twitteroauth/db1.php
+++ src/web/twitteroauth/db1.php
@@ -1,14 +1,13 @@
 <?php
-
-
 require_once ('MDB2.php');
-
-$dsn = "mysqli://twical:7z3g4sYvqU43Nk@localhost/twical";
+$dsn = "mysqli://root:rooroo06@localhost/twical";
 $conn = MDB2::connect ($dsn);
    if (PEAR::isError ($conn)){
        die ("MDB2 Error - Cannot connect: " . $conn->getUserInfo () . "\n");
        }
 $qry = "insert into `twical`.`Person` ( `account_name`, `id`, `twitter_userid`, `twitter_secret`, `twitter_token`, `calendar_url`, `is_muted`) values ( '".$_SESSION['access_token']['oauth_token']."', '0', '97', '79797', '96969876', 'jgkjgjgjhgj', '1')";
-echo $qry;
-
+$conn->query($qry);
+   if (PEAR::isError ($conn)){
+       die ("MDB2 Error - Cannot connect: " . $conn->getUserInfo () . "\n");
+       }
 ?>
