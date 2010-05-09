7f99c0f59de86b38dc8c2043917dff14b373e2f3
Nathan O'Hanlon <nathan@ProsperTechnologies.com>
2010-05-09 11:36:07 +0100

    latest

--- src/apps/frontend/modules/default/actions/actions.class.php
+++ src/apps/frontend/modules/default/actions/actions.class.php
@@ -38,7 +38,7 @@ class defaultActions extends sfActions {
       //look for a person with this guard user id already.
       $q = Doctrine_Query::create()
         ->from('Person p')
-        ->where('p.sf_guard_user_id == ?', $guardUserId);
+        ->where('p.sf_guard_user_id = ?', $guardUserId);
 
       $person = $q->execute();
 
