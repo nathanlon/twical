364ca00b835919bcd0e708ff7cfe823fab024a86
Ketan Majmudar <ket@spiritquest.co.uk>
2010-05-09 13:11:12 +0100

    various file css updates and the old twitterauth setup

--- src/web/twitteroauth/config.php
+++ src/web/twitteroauth/config.php
@@ -7,4 +7,4 @@
 
 define('CONSUMER_KEY', 'j1f0PAgq23BQpHfqu1I3dA');
 define('CONSUMER_SECRET', 'ntG04nVidtg5SjEHRcNvjfq6iUfydZIjyD6u9I');
-define('OAUTH_CALLBACK', 'http://twical.localdomain/twitteroauth/callback.php');
+define('OAUTH_CALLBACK', 'http://twical.net/twitteroauth/callback.php');
