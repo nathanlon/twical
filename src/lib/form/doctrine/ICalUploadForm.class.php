df66f8f9b402c0087fcb9beef806ccd3f3c2b209
Nathan O'Hanlon <nathan@ProsperTechnologies.com>
2010-05-15 15:05:22 +0100

    adding an upload ical file form

--- src/lib/form/doctrine/ICalUploadForm.class.php
+++ src/lib/form/doctrine/ICalUploadForm.class.php
@@ -1,10 +1,14 @@
 <?php
 
-class ICalUploadForm extends sfFormObject
+class ICalUploadForm extends PersonForm
 {
 
   public function configure()
   {
+    unset($this['id'], $this['is_muted'], $this['twitter_token'], $this['twitter_secret'],
+      $this['twitter_userid'], $this['account_name'], $this['calendar_url'], $this['sf_guard_user_id'],
+      $this['created_at'], $this['updated_at']);
+    
     $this->widgetSchema['upload'] = new sfWidgetFormInputFile();
   }
 
